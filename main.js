55063d7a1091dfe4fd8fd3baf32e5560111c7a04
vladikgolosnoi <151874364+vladikgolosnoi@users.noreply.github.com>
2024-08-27 20:36:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// var myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+
+// document.querySelector("html").onclick = function () {
+//     alert("Ouch! Stop poking me!");
+//   };
+
+
+// Image switcher code
+
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === '../images/firefox-icon.png') {
+    myImage.setAttribute ('src','../images/firefox2.png');
+  } else {
+    myImage.setAttribute ('src','../images/firefox-icon.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Пожалуйста введите ваше имя.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Мозила крутая, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Мозила крутая, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
